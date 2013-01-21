a2ee936cce49809b0b90424aa0061156529c8d9f
dkaplan88 <dkaplan@g2labs.net>
2013-01-20 21:25:09 -0600

    chapter 7 done

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 module Codebreaker 
   describe Game do
+    let(:output) { double('output').as_null_object }
+    let(:game) { Game.new(output) }
+
     describe "#start" do
-      let(:output) { double('output').as_null_object }
-      let(:game) { Game.new(output) }
-      
       it "sends a welcome message" do 
         output.should_receive(:puts).with('Welcome to Codebreaker!')
         game.start('1234')
@@ -13,6 +13,47 @@ module Codebreaker
       it "prompts for the first guess" do 
         output.should_receive(:puts).with('Enter guess:')
         game.start('1234')
+      end
+    end
+
+    describe '#guess' do 
+      before :each do 
+        game.start('1234')
+      end
+
+      context 'with no matches' do 
+        it "sends a mark with ''" do 
+          output.should_receive(:puts).with('')
+          game.guess('5555')
+        end
+      end
+
+      context 'with 1 number match' do 
+        it "sends a mark with '_'" do 
+          output.should_receive(:puts).with('-')
+          game.guess('2555')
+        end
+      end
+
+      context 'with 1 exact match' do 
+        it "sends a mark with '+'" do 
+         output.should_receive(:puts).with('+')
+         game.guess('1555')
+        end
+      end
+
+      context 'with 2 number matches' do 
+        it "sends a mark with '--'" do 
+          output.should_receive(:puts).with('--')
+          game.guess('3199')
+        end
+      end
+
+      context 'with 1 number match and 1 exact match(in that order)' do 
+        it "sends a mark with '+-'" do 
+          output.should_receive(:puts).with('+-')
+          game.guess('2930')
+        end
       end
     end
   end
