2c4e075f03c2bed4a86a3580a8113f0892823989
dkaplan88 <dkaplan@g2labs.net>
2013-02-02 22:00:50 -0600

    chapter 9 done

--- lib/codebreaker/marker.rb
+++ lib/codebreaker/marker.rb
@@ -3,15 +3,25 @@ class Marker
     @secret, @guess = secret, guess
   end
 
-  def exact_match_count
-    (0..3).inject(0) do |count, index|
-      count + (exact_match?(index) ? 1 : 0)
+  def total_match_count
+    count = 0
+    secret = @secret.split('')
+    @guess.split('').inject(0) do |count, n|
+      count + (delete_first(secret, n) ? 1 : 0)
     end
   end
 
+  def delete_first(code,n)
+    code.delete_at(code.index(n)) if code.index(n)
+  end
+
   def number_match_count
+    total_match_count - exact_match_count
+  end
+
+  def exact_match_count
     (0..3).inject(0) do |count, index|
-      count + (number_match?(index) ? 1 : 0)
+      count + (exact_match?(index) ? 1 : 0)
     end
   end
 
